44376b31a0f5aaa30d59d006e929c0efe80b3c97
marb <marb61a@gmail.com>
2016-05-02 13:04:48 +0000

    updated app.js

--- app.js
+++ app.js
@@ -30,6 +30,45 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Handle Sessions
+app.use(session({
+    secret:'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Validator
+app.use(expressValidator({
+errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.'), 
+    root    = namespace.shift(),
+    formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
 // Routes
 app.use('/', routes);
 app.use('/albums', albums);
