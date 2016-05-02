eb222d2218e4be53550191535c334b3ec099d959
marb <marb61a@gmail.com>
2016-05-02 12:44:07 +0000

    updated folder structure and app.js

--- app.js
+++ app.js
@@ -14,3 +14,32 @@ var routes = require('./routes/index');
 var albums = require('./routes/albums');
 var genres = require('./routes/genres');
 var users = require('./routes/users');
+
+// Init App
+var app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Logger
+app.use(logger('dev'));
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Routes
+app.use('/', routes);
+app.use('/albums', albums);
+app.use('/genres', genres);
+app.use('/users', users);
+
+// Set Port
+app.set('port', (process.env.PORT || 3000));
+
+// Run Server
+app.listen(app.get('port'), function(){
+  console.log('Server started on port: '+app.get('port'));
+});
