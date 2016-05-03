a1e571746953da3b029b01149248fb67969f48f8
marb <marb61a@gmail.com>
2016-05-03 13:24:24 +0000

    updated genres routse and added albums index view

--- routes/genres.js
+++ routes/genres.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var Firebase = require('firebase');
+var fbRef = new Firebase('');
 
 router.get('/', function(req, res, next) {
   	res.render('genres/index');
+});
+
+router.get('/add', function(req, res, next) {
+  	res.render('genres/add');
+});
+
+router.post('./add', function(req, res, naxt){
+    var genre = {
+        name : req.body.name
+    };
+    var genreRef = fbRef.child('genres');
+  	genreRef.push().set(genre);
+
+  	req.flash('success_msg', 'Genre Saved');
+  	res.redirect('/genres');
 });
 
 module.exports = router;
