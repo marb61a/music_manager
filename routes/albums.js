ccc3c3e38064ae57d031abdc3a094bc425e8a752
marb <marb61a@gmail.com>
2016-05-03 13:13:58 +0000

    added get function to albums route

--- routes/albums.js
+++ routes/albums.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var Firebase = require('firebase');
+var fbRef = new Firebase('');
+var multer = require('multer');
+var upload = multer({dest:'./public/images/uploads'});
 
 router.get('/', function(req, res, next) {
   	res.render('albums/index');
+});
+
+router.get('./add', function(req, res, next) {
+    var genreRef = fbRef.child('genres');
+
+	genreRef.once('value', function(snapshot){
+		var data = [];
+		snapshot.forEach(function(childSnapshot){
+			var key = childSnapshot.key();
+			var childData = childSnapshot.val();
+			data.push({
+				id: key,
+				name: childData.name
+			});
+		});
+		res.render('albums/add',{genres: data});
+	});
 });
 
 module.exports = router;
